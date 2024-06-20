f7f60e1bae8ca3866b9bb4c6c1d50b483cce39a2
oldmartijntje <oldmartijntje@gmail.com>
2024-06-20 13:24:16 +0200

    ported from my own api

---
+++ homepage/script.js
@@ -0,0 +1,71 @@
+const endpoints = [
+    {
+        name: 'isOnline',
+        url: 'https://api.oldmartijntje.nl/test/isOnline',
+        type: 'GET',
+        header: 'Random',
+        hoverText: 'This endpoint is used to check if the server is online.',
+    },
+    {
+        name: 'visits',
+        url: 'https://api.oldmartijntje.nl/test/visits',
+        type: 'GET',
+        header: 'Random',
+        hoverText: 'This endpoint is used to check how many times the server has been visited.',
+    }
+];
+
+const tabInfo = {
+    "user": "these endpoints require the user to be logged in. And currently I am the only one with an account.",
+    "admin": "these endpoints require the logged in user to have admin privilages.",
+    "Random": "This is just for testing purposes and should not be used in production.",
+    "Authorization": "These endpoints are for authorization purposes. This can be for actual accounts or <a href=''>single use tokens</a>."
+}
+
+const apiList = document.getElementById('api-list');
+
+function addEndpoint(name, url, type, header, hoverText) {
+    const headerElement = document.getElementById(header);
+
+    if (headerElement) {
+        const headerElementList = document.getElementById(`${header}-list`);
+        // Header already exists, append to it
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<span><b>${type}</b> - </span><span><b>${name}</b>: </span><a href="${url}">${url}</a><b style="cursor:pointer" class="icon" title="${hoverText}" onclick="alert('${hoverText}')">ⓘ</b>`;
+
+        headerElementList.appendChild(listItem);
+    } else {
+        // Create a new header and list
+        const headerElement = document.createElement('div');
+        const headerInfoElement = document.createElement('div');
+        const headerHeaderElement = document.createElement('h2');
+        headerElement.id = header;
+        headerHeaderElement.textContent = header;
+        apiList.appendChild(headerElement);
+        headerElement.appendChild(headerHeaderElement);
+
+        headerInfoElement.id = `${header}-info`;
+        headerElement.appendChild(headerInfoElement);
+
+        const list = document.createElement('ul');
+        list.id = `${header}-list`;
+        headerElement.appendChild(list);
+
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<span><b>${type}</b> - </span><span><b>${name}</b>: </span><a href="${url}">${url}</a><b style="cursor:pointer" class="icon" title="${hoverText}" onclick="alert('${hoverText}')">ⓘ</b>`;
+        list.appendChild(listItem);
+    }
+}
+
+for (const endpoint of endpoints) {
+    addEndpoint(endpoint.name, endpoint.url, endpoint.type, endpoint.header, endpoint.hoverText);
+}
+
+for (const header in tabInfo) {
+    const headerElement = document.getElementById(`${header}-info`);
+    if (headerElement) {
+        const headerInfoElement = document.createElement('p');
+        headerInfoElement.innerHTML = tabInfo[header];
+        headerElement.appendChild(headerInfoElement);
+    }
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+const cors = require("cors");
+const path = require('path');
+const express = require("express");
+const expressStatic = require('express').static;
+const settings = require("./settings");
+
+const port = settings.apiPort;
+const staticHtmlPath = path.join(__dirname, './homepage');
+
+var visit = 0;
+
+const testRouter = express.Router();
+testRouter.use(express.json());
+
+testRouter.get("/isOnline", async (_req, res) => {
+    visit++;
+    console.log("Connection go brrr...")
+    res.status(200).send({ "message": "The server is online." });
+});
+
+testRouter.get("/visits", async (_req, res) => {
+    visit++;
+    console.log("We have had " + visit + " visits since startup!")
+    res.status(200).send({ "message": "We have had " + visit + " visits since startup!" });
+});
+
+
+
+const app = express();
+app.use(cors());
+app.use(expressStatic(staticHtmlPath));
+app.use("/test", testRouter);
+// start the Express server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}...`);
+}).on('error', (err) => {
+    console.error('Server startup error:', err);
+});
